8c1feb0cdf4dceaae12f012b0c69f5d83804227d
dglipp <dg.lippolis@hotmail.it>
2020-08-30 18:18:26 +0200

    graphs modified

--- NeuralNet_class/Regression.py
+++ NeuralNet_class/Regression.py
@@ -8,7 +8,6 @@ net = nn.Net(1, [1000, 1], [nn.sigmoid_activation] + [tf.nn.softmax], nn.MSE_los
 X = np.linspace(-1,1, 100).reshape((100, 1))
 X = (X-np.mean(X, axis = 0))/np.std(X, axis = 0)
 y_real = np.exp(3*X)
-
 lr = 1e-2
 model = nn.Trainer(net, X, y_real, nn.Sgd(lr))
 tl,vl = model.train(3, 1, learning_rate=lr)
@@ -18,8 +17,10 @@ plt.plot(X, y_pred, "o", label ="pred", markersize = 1)
 plt.plot(X, y_real, "o", label ="real", markersize = 1)
 plt.legend()
 plt.show()
-plt.figure()
-plt.plot(tl, label="train_loss")
-plt.plot(vl, label="validate_loss")
-plt.legend()
+
+fig, ax = plt.subplots(1, 2)
+ax[0].plot(tl)
+ax[1].plot(vl)
+ax[0].set_title("Train loss")
+ax[1].set_title("Validation loss")
 plt.show()
