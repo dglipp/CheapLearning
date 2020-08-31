cff2ae90668aeb6b416f958e9f961be46e9389b1
dglipp <dg.lippolis@hotmail.it>
2020-08-31 10:53:34 +0200

    minor mods

--- NeuralNet_class/Regression.py
+++ NeuralNet_class/Regression.py
@@ -3,14 +3,14 @@ import numpy as np
 import tensorflow as tf
 import NeuralNet_class as nn
 
-net = nn.Net(1, [1000, 1], [nn.sigmoid_activation] + [tf.nn.softmax], nn.MSE_loss)
+net = nn.Net(1, [1000, 1], [nn.sigmoid_activation] + [tf.identity], nn.MSE_loss)
 
 X = np.linspace(-1,1, 100).reshape((100, 1))
 X = (X-np.mean(X, axis = 0))/np.std(X, axis = 0)
 y_real = np.exp(3*X)
 lr = 1e-2
 model = nn.Trainer(net, X, y_real, nn.Sgd(lr))
-tl,vl = model.train(3, 1, learning_rate=lr)
+tl,vl = model.train(50, 1, learning_rate=lr)
 y_pred = model.net.forward_pass(X)
 plt.figure()
 plt.plot(X, y_pred, "o", label ="pred", markersize = 1)
